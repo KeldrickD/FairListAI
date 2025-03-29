2b61f3ace01b7ad8e248622c866e0cfb6ff044df
Your Name <your.email@example.com>
2025-03-29 00:52:25 -0400

    Fix duplicate layout issue by removing redundant Layout wrapper from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { Toaster } from '@/components/ui/toaster';
-import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
       <Toaster />
     </>
   );
